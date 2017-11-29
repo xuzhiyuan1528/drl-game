7735a6f91e361b99e7ff72d1c4a9cf78b47d80f2
JoshuaTang <1240604020@qq.com>
2017-11-29 14:33:46 -0500

    update pong game with args

--- dqn-half-pong-org.py
+++ dqn-half-pong-org.py
@@ -1,15 +1,31 @@
 from Agent.dqn_half_pong_agent import DeepQHalfPongPlayer
+import argparse
+
 
 if __name__ == '__main__':
-    # Here, it loads the trained model and plays the game.
-    # Please note that the model was trained with 40*40 grid at 8 frames per
-    # second, so the game window is very small.
-    player = DeepQHalfPongPlayer(
-        checkpoint_path='./Res/deep_q_half_pong_networks_40x40_8',
-        playback_mode=False
-    )
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '-width', help='screen width', nargs='?',
+        type=int, const=40, default=40)
+    parser.add_argument(
+        '-height', help='screen height', nargs='?',
+        type=int, const=40, default=40)
+    parser.add_argument('-train', help='train the model', action='store_true')
+    args = parser.parse_args()
 
-    # To train the model, uncomment the following code.
-    # player = DeepQHalfPongPlayer()
+    DeepQHalfPongPlayer.SCREEN_WIDTH = args.width
+    DeepQHalfPongPlayer.SCREEN_HEIGHT = args.height
+
+    if args.train:
+        # To train the model.
+        player = DeepQHalfPongPlayer()
+    else:
+        # Here, it loads the trained model and plays the game.
+        # Please note that the model was trained with 40*40 grid at 8 frames per
+        # second, so the game window is very small.
+        player = DeepQHalfPongPlayer(
+            checkpoint_path='./Res/deep_q_half_pong_networks_40x40_8',
+            playback_mode=False
+        )
 
     player.start()
